23429fa27e53d71449c73dcaeff12c4b7a6bb489
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-07 15:37:32 +0200

    add TypeORM config for connection to mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'flair',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
