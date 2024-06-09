5c7fb5c816ee3e32cafe261d3bce237b190e9995
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-10 01:06:50 +0200

    add validation decorators

--- src/transaction/model/transaction.model.ts
+++ src/transaction/model/transaction.model.ts
@@ -1,4 +1,5 @@
 import { Field, Float, ID, ObjectType } from '@nestjs/graphql';
+import { IsDate, IsNumber, IsString, Length, Max, Min } from 'class-validator';
 
 @ObjectType()
 export class Transaction {
@@ -6,23 +7,38 @@ export class Transaction {
   id: string;
 
   @Field()
+  @IsDate()
   startedDate: Date;
 
   @Field()
+  @IsDate()
   completedDate: Date;
 
   @Field(() => Float)
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @Min(0)
+  @Max(999999999999.99)
   startBalance: number;
 
   @Field(() => Float)
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @Min(0)
+  @Max(999999999999.99)
   endBalance: number;
 
   @Field()
+  @IsString()
+  @Length(1, 255)
   description: string;
 
   @Field(() => Float)
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @Min(0)
+  @Max(999999999999.99)
   amount: number;
 
   @Field()
+  @IsString()
+  @Length(3, 3)
   currency: string;
 }
