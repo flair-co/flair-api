c028aba28fc9ce03fff72376909b3c2d03f7c7b3
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-10 02:33:58 +0200

    add csv implementation of FileParser

--- src/file-parsers/file-parser.factory.ts
+++ src/file-parsers/file-parser.factory.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { FileParser } from './file-parser.interface';
 import { XlsFileParser } from './service/xls-file-parser.service';
+import { CsvFileParser } from './service/csv-file-parser.service';
 
 @Injectable()
 export class FileParserFactory {
-  constructor(private readonly xlsParser: XlsFileParser) {}
+  constructor(
+    private readonly xlsParser: XlsFileParser,
+    private readonly csvParser: CsvFileParser,
+  ) {}
 
   create(mimetype: string): FileParser {
     switch (mimetype) {
       case 'application/vnd.ms-excel':
         return this.xlsParser;
+      case 'text/csv':
+        return this.csvParser;
       default:
         throw new Error(`Unsupported file type: ${mimetype}`);
     }
--- src/file-parsers/file-parser.module.ts
+++ src/file-parsers/file-parser.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { FileParserFactory } from './file-parser.factory';
 import { XlsFileParser } from './service/xls-file-parser.service';
+import { CsvFileParser } from './service/csv-file-parser.service';
 import { FileParserResolver } from './file-parser.resolver';
 import { TransactionAdapterModule } from 'src/bank-transaction-adapter/bank-transaction-adapter.module';
 
 @Module({
   imports: [TransactionAdapterModule],
-  providers: [XlsFileParser, FileParserFactory, FileParserResolver],
+  providers: [
+    XlsFileParser,
+    CsvFileParser,
+    FileParserFactory,
+    FileParserResolver,
+  ],
 })
 export class FileParserModule {}
