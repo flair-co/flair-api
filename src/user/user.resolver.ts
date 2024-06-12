800b9768bb5c69bdfe2e51df3bc69740c458afeb
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-12 18:05:48 +0200

    remove async keyword

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -7,12 +7,12 @@ export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
   @Query(() => [User])
-  async users(): Promise<User[]> {
+  users(): Promise<User[]> {
     return this.userService.findAll();
   }
 
   @Query(() => User)
-  async user(@Args('id') id: string): Promise<User | null> {
-    return this.userService.findOne(id);
+  user(@Args('id') id: string): Promise<User | null> {
+    return this.userService.findById(id);
   }
 }
