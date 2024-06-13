9a13e47017aea4545f698ae27a109791709de70c
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-13 22:24:26 +0200

    extend AuthGuard('local') to update request with loginInput args

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,19 +1,24 @@
-import { Resolver, Mutation } from '@nestjs/graphql';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { User } from 'src/user/models/user.model';
 import { UseGuards } from '@nestjs/common';
 import { Public } from './decorators/public.decorator';
 import { CurrentUser } from './decorators/current-user.decorator';
-import { AuthGuard } from '@nestjs/passport';
+import { AccessToken } from './outputs/access-token.output';
+import { LoginInput } from './inputs/login.input';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Resolver(() => User)
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
   @Public()
-  @UseGuards(AuthGuard('local'))
-  @Mutation(() => User)
-  async login(@CurrentUser() user: User) {
-    return this.authService.login(user);
+  @UseGuards(LocalAuthGuard)
+  @Mutation(() => AccessToken)
+  logIn(
+    @Args('loginInput') _loginInput: LoginInput,
+    @CurrentUser() user: User,
+  ) {
+    return this.authService.signAccessToken(user);
   }
 }
