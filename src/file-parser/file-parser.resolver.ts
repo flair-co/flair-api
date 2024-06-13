66613c4edbb6f629c1a1126b2650eb8a8490ac75
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-13 19:35:32 +0200

    extract away logic from resolver into service

--- src/file-parser/file-parser.resolver.ts
+++ src/file-parser/file-parser.resolver.ts
@@ -1,16 +1,12 @@
 import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { GraphQLUpload, FileUpload } from 'graphql-upload';
-import { FileParserFactory } from './file-parser.factory';
 import { Bank } from 'src/bank-transaction-adapter/constant/bank';
 import { Transaction } from 'src/transaction/model/transaction.model';
-import { BankTransactionAdapterFactory } from 'src/bank-transaction-adapter/bank-transaction-adapter.factory';
+import { FileParserService } from './file-parser.service';
 
 @Resolver()
 export class FileParserResolver {
-  constructor(
-    private readonly fileParserFactory: FileParserFactory,
-    private readonly bankTransactionAdapterFactory: BankTransactionAdapterFactory,
-  ) {}
+  constructor(private readonly fileParserService: FileParserService) {}
 
   @Mutation(() => [Transaction])
   async parse(
@@ -18,19 +14,7 @@ export class FileParserResolver {
     file: FileUpload,
     @Args('bank') bank: Bank,
   ) {
-    const { createReadStream, mimetype } = file;
-    const stream = createReadStream();
-    const chunks: Buffer[] = [];
-
-    for await (const chunk of stream) {
-      chunks.push(chunk);
-    }
-    const buffer = Buffer.concat(chunks);
-
-    const data = this.fileParserFactory.create(mimetype).parse(buffer);
-    const transactions = this.bankTransactionAdapterFactory
-      .create(bank)
-      .map(data);
+    const transactions = await this.fileParserService.parse(file, bank);
 
     return transactions;
   }
---
+++ src/file-parser/file-parser.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { FileParserFactory } from './file-parser.factory';
+import { FileUpload } from 'graphql-upload';
+import { BankTransactionAdapterFactory } from 'src/bank-transaction-adapter/bank-transaction-adapter.factory';
+import { Transaction } from 'src/transaction/model/transaction.model';
+import { Bank } from 'src/bank-transaction-adapter/constant/bank';
+import { ReadStream } from 'fs-capacitor';
+
+@Injectable()
+export class FileParserService {
+  constructor(
+    private readonly fileParserFactory: FileParserFactory,
+    private readonly bankTransactionAdapterFactory: BankTransactionAdapterFactory,
+  ) {}
+
+  async parse(file: FileUpload, bank: Bank): Promise<Transaction[]> {
+    const { createReadStream, mimetype } = file;
+    const stream = createReadStream();
+
+    const buffer = await this.readStream(stream);
+
+    const fileParser = this.fileParserFactory.create(mimetype);
+    const data = fileParser.parse(buffer);
+
+    const adapter = this.bankTransactionAdapterFactory.create(bank);
+    const transactions = adapter.map(data);
+
+    return transactions;
+  }
+
+  private async readStream(stream: ReadStream): Promise<Buffer> {
+    const chunks: Buffer[] = [];
+    for await (const chunk of stream) {
+      chunks.push(chunk);
+    }
+    return Buffer.concat(chunks);
+  }
+}
