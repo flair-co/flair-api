3f189320782220ee68ff2c8d09a35dd4a8bec4a2
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-13 22:25:42 +0200

    refactor style for function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import * as argon2 from 'argon2';
+import { AccessToken } from './outputs/access-token.output';
 
 @Injectable()
 export class AuthService {
@@ -19,13 +20,13 @@ export class AuthService {
     return null;
   }
 
-  async login(user: any) {
+  async signAccessToken(user: any): Promise<AccessToken> {
     const payload = {
       sub: user.id,
       email: user.email,
     };
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-    };
+
+    const jwt = await this.jwtService.signAsync(payload);
+    return { access_token: jwt };
   }
 }
