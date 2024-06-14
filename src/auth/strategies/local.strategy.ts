7bea457bdb13529c24f5173b9f10d4f8ccdc16b8
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-14 16:07:05 +0200

    fix return type to User

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -2,6 +2,7 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
+import { User } from 'src/user/models/user.model';
 
 /**
  * Implements local authentication strategy via AuthService (email/password).
@@ -12,7 +13,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     super({ usernameField: 'email' });
   }
 
-  async validate(email: string, password: string): Promise<any> {
+  async validate(email: string, password: string): Promise<User> {
     const user = await this.authService.validateUser(email, password);
 
     if (!user) {
