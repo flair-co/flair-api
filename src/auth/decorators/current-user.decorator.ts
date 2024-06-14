444162557c84a4c5ad79f795c043af308981369e
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-14 16:05:35 +0200

    pass data parameter to fix undefined context error

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -4,7 +4,9 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 /**
  * Extracts the current user from the request context.
  */
-export const CurrentUser = createParamDecorator((context: ExecutionContext) => {
-  const ctx = GqlExecutionContext.create(context);
-  return ctx.getContext().req.user;
-});
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req.user;
+  },
+);
