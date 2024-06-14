26d3a07a4adfd38cd5a2b432499e89052f3a797a
eduard-cc <eduard.ciociu22@gmail.com>
2024-06-14 16:04:19 +0200

    add createUser method and update return types

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import * as argon2 from 'argon2';
-import { AccessToken } from './outputs/access-token.output';
+import { AccessToken } from './dto/access-token.output';
+import { plainToClass } from 'class-transformer';
+import { User } from 'src/user/models/user.model';
+import { CreateUserArgs } from 'src/user/dto/create-user.args';
 
 @Injectable()
 export class AuthService {
@@ -11,7 +14,7 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async validateUser(email: string, password: string): Promise<any> {
+  async validateUser(email: string, password: string): Promise<User | null> {
     const user = await this.userService.findByEmail(email);
 
     if (user && (await argon2.verify(user.password, password))) {
@@ -20,7 +23,22 @@ export class AuthService {
     return null;
   }
 
-  async signAccessToken(user: any): Promise<AccessToken> {
+  async createUser(createUserArgs: CreateUserArgs): Promise<User> {
+    const existingUser = await this.userService.findByEmail(
+      createUserArgs.email,
+    );
+    if (existingUser) {
+      throw new Error('User already exists');
+    }
+
+    const hash = await argon2.hash(createUserArgs.password);
+    createUserArgs.password = hash;
+
+    const user = await this.userService.create(createUserArgs);
+    return plainToClass(User, user);
+  }
+
+  async signAccessToken(user: User): Promise<AccessToken> {
     const payload = {
       sub: user.id,
       email: user.email,
