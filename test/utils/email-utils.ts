7d70484a0f8d1e5c4dd60518de7069017eb63e19
eduard-cc <eduard.ciociu22@gmail.com>
2025-05-28 15:13:35 +0200

    refactor e2e test constants, types and email utils class

---
+++ test/utils/email-utils.ts
@@ -0,0 +1,91 @@
+import ms from 'ms';
+
+import {Account} from '@modules/account/account.entity';
+
+import {MailpitMessageSummaryResponse, MailpitResponse} from '../types/mailpit';
+import {SIX_DIGIT_REGEX, UUID_REGEX} from '../types/regex.constants';
+
+export class EmailUtils {
+	static async clearEmails(apiUrl: string) {
+		await fetch(`${apiUrl}/api/v1/messages`, {method: 'DELETE'});
+	}
+
+	static async findEmailByRecipient(recipientEmail: string, apiUrl: string) {
+		let retries = 10;
+		const delayMs = 200;
+
+		// retry briefly to allow BullMQ to process the job and send email to Mailpit
+		while (retries > 0) {
+			const res = await fetch(`${apiUrl}/api/v1/messages`);
+			const data: MailpitResponse = await res.json();
+			const email = data.messages.find((msg) =>
+				msg.To.some((r) => r.Address.toLowerCase() === recipientEmail.toLowerCase()),
+			);
+
+			if (email) {
+				const res = await fetch(`${apiUrl}/api/v1/message/${email.ID}`);
+				const fullEmail: MailpitMessageSummaryResponse = await res.json();
+				return fullEmail;
+			}
+
+			retries--;
+			if (retries > 0) await this._sleep(delayMs);
+		}
+	}
+
+	static extractVerificationCode(body?: string) {
+		if (!body) return '';
+		const pattern = new RegExp(`You can also manually enter the code below:·?\\s*(${SIX_DIGIT_REGEX.source})`, 'i');
+		const match = body.match(pattern);
+		return match ? match[1] : '';
+	}
+
+	static extractPasswordResetToken(body?: string) {
+		if (!body) return null;
+
+		// First, decode the quoted-printable encoding. Replaces =3D with =
+		let decodedBody = body.replace(/=3D/g, '=');
+		// Handle line breaks with = at the end
+		decodedBody = decodedBody.replace(/=\r?\n/g, '');
+
+		const pattern = new RegExp(`reset-password\\?token=${UUID_REGEX.source}`);
+		const match = decodedBody.match(pattern);
+		if (match) {
+			const uuidMatch = match[0].match(UUID_REGEX);
+			return uuidMatch ? uuidMatch[0] : null;
+		}
+		return null;
+	}
+
+	static normalizeEmailText(text?: string) {
+		if (!text) return '';
+		// remove all asterisk characters
+		let cleaned = text.replace(/\*/g, '');
+		// normalize all newline types
+		cleaned = cleaned.replace(/\r\n/g, '\n');
+		// remove all blank lines
+		cleaned = cleaned.replace(/\n+/g, '\n');
+		return cleaned.trim();
+	}
+
+	static getWelcomeEmailBody(
+		email: Account['email'],
+		name: Account['name'],
+		webUrl: string,
+		code: string,
+		expiration: string,
+	) {
+		const ex = ms(ms(expiration), {long: true});
+
+		return `Welcome to Flair, ${name}.
+Please confirm your email address by clicking the button below.
+Verify email ( ${webUrl}/verify-email?email=${encodeURIComponent(email)}&code=${code}&flow=onboarding )
+This link and code expire in ${ex}. You can also manually enter the code below:
+${code}
+If you did not sign up for Flair, please disregard this email.`;
+	}
+
+	private static _sleep(ms: number = 250) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
+}
